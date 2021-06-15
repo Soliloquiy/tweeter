e404b7d8d5b7ec273e20dd5cb65d3c74e1c605e6
Soliloquy <ibrahia@ucalgary.ca>
2021-06-15 22:37:13 +0000

    character counter complete

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
   const textArea = $("#tweet-text");
   // const counter = document.getElementById("counter");
   textArea.on("keypress", function(event) {
-    console.log($(this).val());
     const counter = $(this).closest(".new-tweet").find(".counter")
     $(counter).val((140 - (this.value.length + 1)));
-    // console.log(this.parent())
-    //find children after parent
-    // console.log(this.value.length);
-    // counter.value = (140 - (this.value.length + 1))
-    
-  })
 
+    //add red to new-tweet
+    if ($(this).val().length > 140) {
+      counter.addClass("add-red-to-new-tweet");
+    } else {
+      counter.removeClass("add-red-to-new-tweet")
+    }
+  })
 
 });
